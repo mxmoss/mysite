adfea30a54e8c5aa1cb437f2eabe66dd6021b025
mxmoss <mxmossman@gmail.com>
2016-10-07 23:53:55 -0700

    Display actual data
    The post_list is hot. displays data from database

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from .models import myRecord
 
 def index(request):
   return HttpResponse("Hello, world. You're at the myapp index.")
 	
 def post_list(request):
-    return render(request, 'myapp/post_list.html', {})
+    records = myRecord.objects.filter().order_by('created_date')
+#    records = myRecord.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'myapp/post_list.html', {'records': records})
