6f070a12ed4f2887a716eb240da803dc9835b9cf
mxmoss <mxmossman@gmail.com>
2016-10-08 22:23:08 -0700

    Added "New" and "Edit" functions
    Added "New" link to post_detail
    Added ability to edit a record

--- myapp/views.py
+++ myapp/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.utils import timezone
 from .models import myRecord
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.shortcuts import redirect
 
 def index(request):
   return HttpResponse("Hello, world. You're at the myapp index.")
@@ -15,3 +17,30 @@ def post_list(request):
 def post_detail(request, pk):
     record = get_object_or_404(myRecord, pk=pk)
     return render(request, 'myapp/post_detail.html', {'record': record})
+		
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'myapp/post_edit.html', {'form': form})
+		
+def post_edit(request, pk):
+    record = get_object_or_404(myRecord, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=record)
+        if form.is_valid():
+            record = form.save(commit=False)
+            record.author = request.user
+            record.published_date = timezone.now()
+            record.save()
+            return redirect('post_detail', pk=record.pk)
+    else:
+        form = PostForm(instance=record)
+    return render(request, 'myapp/post_edit.html', {'form': form})
