3b0162c9d23c2df044a4d6cb40715d0b66ecd739
mxmoss <mxmossman@gmail.com>
2016-10-08 20:44:48 -0700

    Added post detail form
    Added post detail form

--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
 from .models import myRecord
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
   return HttpResponse("Hello, world. You're at the myapp index.")
@@ -10,3 +11,7 @@ def post_list(request):
     records = myRecord.objects.filter().order_by('created_date')
 #    records = myRecord.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'myapp/post_list.html', {'records': records})
+
+def post_detail(request, pk):
+    record = get_object_or_404(myRecord, pk=pk)
+    return render(request, 'myapp/post_detail.html', {'record': record})
